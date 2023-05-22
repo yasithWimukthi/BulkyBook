5ab75ffa2becce4987a0fafc971a5386f5cbf861
yasith <wimukthiyasith@gmail.com>
2023-05-22 23:46:57 +0530

    create category page

--- Bulky Book Web/Controllers/CategoryController.cs
+++ Bulky Book Web/Controllers/CategoryController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bulky_Book_Web.Models;
+using BulkyBookWeb.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bulky_Book_Web.Controllers
 {
     public class CategoryController : Controller
     {
+
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
         }
     }
 }
